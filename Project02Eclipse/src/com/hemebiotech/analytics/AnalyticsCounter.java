f09cfc2ef3c49921c883f8e4b5e0c036203a1950
Bak <bbakam.pro@gmail.com>
2021-02-18 21:43:12 +0100

    création de méthodes et structuration des exceptions

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,28 +3,48 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private String inputFilePath;
-	private static String outputFilePath;
-
+	private String outputFilePath;
+	Map<String, Integer> symptomsOccurence = new TreeMap<String, Integer>();
 	// TreeMap permet de classer les éléments de ma map dans l'ordre alphabétique
-	static Map<String, Integer> symptomsOccurence = new TreeMap<String, Integer>();
 
-	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-		FileWriter writer = new FileWriter(outputFilePath);
-		String line = reader.readLine();
-		reader.close();
+	public void saveResult() {
+		try {
+			FileWriter writer = new FileWriter(outputFilePath);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 
+	public Map<String, Integer> readSymptoms() {
+		try (BufferedReader reader = new BufferedReader(new FileReader(this.inputFilePath));) {
+			String line = reader.readLine();
+			while (line != null) {
+				if (symptomsOccurence.containsKey(line)) {
+					symptomsOccurence.put(line, symptomsOccurence.get(line) + 1);
+				} else {
+					symptomsOccurence.put(line, 1);
+				}
+				line = reader.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Le fichier n'a pas pu être lu" + e.getMessage());
+		}
+		return symptomsOccurence;
+	}
+
+	public AnalyticsCounter(String inputFilePath, String outputFilePath) {
+		super();
+		this.inputFilePath = inputFilePath;
+		this.outputFilePath = outputFilePath;
 	}
 }
-
-// line = reader.readLine(); // get another symptom
-
-// while (line != null) {
 
 // next generate output
 // FileWriter writer = new FileWriter("result.out");
--- Project02Eclipse/src/com/hemebiotech/analytics/Application.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Application.java
@@ -6,6 +6,7 @@ public class Application {
 		String inputFilePath = "Project02Eclipse/symptoms.txt";
 		String outputFilePath = "Project02Eclipse/results.out";
 
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter(inputFilePath, outputFilePath);
 		System.out.println("La liste des symptomes et des occurences" + outputFilePath);
 
 	}
