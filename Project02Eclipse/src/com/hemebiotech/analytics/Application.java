3c955d6533248db10c444541f1be25fa2a1a5522
Bak <bbakam.pro@gmail.com>
2021-02-18 02:34:10 +0100

    création de ma classe Application pour ma fonction main, de mon fichier results.out et suppression des commentaires et variables superflus

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Application.java
@@ -0,0 +1,11 @@
+package com.hemebiotech.analytics;
+
+public class Application {
+
+	public static void main(String[] args) {
+		String inputFilePath = "Project02Eclipse/symptoms.txt";
+		String outputFilePath = "Project02Eclipse/results.out";
+
+	}
+
+}
