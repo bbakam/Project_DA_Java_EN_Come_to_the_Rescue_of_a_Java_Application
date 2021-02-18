4ce9c50cd775b61d9a6cababa8c78e813c6b5758
Bak <bbakam.pro@gmail.com>
2021-02-18 20:13:46 +0100

    intégration de FileReader et BufferedReader

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,20 +2,23 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private String inputFilePath;
-	private String outputFilePath;
-	Map<String, Integer> symptomsOccurence = new TreeMap<String, Integer>();
+	private static String outputFilePath;
+
+	// TreeMap permet de classer les éléments de ma map dans l'ordre alphabétique
+	static Map<String, Integer> symptomsOccurence = new TreeMap<String, Integer>();
 
 	public static void main(String args[]) throws Exception {
-
 		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-
+		FileWriter writer = new FileWriter(outputFilePath);
 		String line = reader.readLine();
 		reader.close();
+
 	}
 }
 
