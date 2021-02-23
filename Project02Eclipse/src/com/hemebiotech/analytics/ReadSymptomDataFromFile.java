89e43d9b6f212273b26960bed771944ed5617ba9
Bak <bbakam.pro@gmail.com>
2021-02-23 04:15:08 +0100

    Ajout du fichier ReadSymptomDataFromFile.java

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -10,7 +10,7 @@ import java.util.List;
  * Simple brute force implementation
  *
  */
-public class ReadSymptomDataFromFile implements ISymptomReader {
+public class ReadSymptomDataFromFile {
 
 	private String filepath;
 
@@ -23,7 +23,6 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 		this.filepath = filepath;
 	}
 
-	@Override
 	public List<String> GetSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
 
@@ -40,9 +39,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 		}
-
 		return result;
 	}
 
